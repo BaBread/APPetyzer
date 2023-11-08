b4b39c911c6ec6f53bcedb2cf193936409f52058
Donald L <dfleon2011@gmail.com>
2023-11-07 19:35:22 -0500

    Home page cards

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -11,24 +11,53 @@ const Home = () => {
 
   return (
     <main>
-      <div className="flex-row justify-center">
-        <div
-          className="col-12 col-md-10 mb-3 p-3"
-          style={{ border: '1px dotted #1a1a1a' }}
-        >
-          <ThoughtForm />
+      <div class="row">
+        <div class="col-sm-3">
+          <a href="data.link to recipe">
+            <img src="data.recipe image" width="95%">data.recipe name</img>
+            <p>data.recipe tags</p>
+          </a>
+          <br></br>
         </div>
-        <div className="col-12 col-md-8 mb-3">
-          {loading ? (
-            <div>Loading...</div>
-          ) : (
-            <ThoughtList
-              thoughts={thoughts}
-              title="Some Feed for Thought(s)..."
-            />
-          )}
+        <div class="col-sm-3">
+          <a href="data.link to recipe">
+            <img src="data.recipe image" width="95%">data.recipe name</img>
+            <p>data.recipe tags</p>
+          </a>
+          <br></br>
+        </div>
+        <div class="col-sm-3">
+          <a href="data.link to recipe">
+            <img src="data.recipe image" width="95%">data.recipe name</img>
+            <p>data.recipe tags</p>
+          </a>
+          <br></br>
         </div>
       </div>
+      <div class="row">
+        <div class="col-sm-3">
+          <a href="data.link to recipe">
+            <img src="data.recipe image" width="95%">data.recipe name</img>
+            <p>data.recipe tags</p>
+          </a>
+          <br></br>
+        </div>
+        <div class="col-sm-3">
+          <a href="data.link to recipe">
+            <img src="data.recipe image" width="95%">data.recipe name</img>
+            <p>data.recipe tags</p>
+          </a>
+          <br></br>
+        </div>
+        <div class="col-sm-3">
+          <a href="data.link to recipe">
+            <img src="data.recipe image" width="95%">data.recipe name</img>
+            <p>data.recipe tags</p>
+          </a>
+          <br></br>
+        </div>
+      </div>
+
     </main>
   );
 };
