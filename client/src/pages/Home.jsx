b2fa19147d81caa635cab010762f0aa742ebfee9
Charles Chavis <chavisca@gmail.com>
2023-11-11 02:43:39 -0500

    massive styling overhaul, added styling to header and footer, login and signup pages, as well as fixed columns and responsiveness to home.jsx and corrected issue with coloring on recipe.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,21 @@
-import { Link } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-import { useQuery } from '@apollo/client';
-import { QUERY_THOUGHTS } from '../utils/queries';
-import { SimpleGrid, Stack, Heading, Divider, Button, Text, Image, Card, CardBody, CardFooter, Link as ChakraLink } from '@chakra-ui/react'
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useQuery } from "@apollo/client";
+import { QUERY_THOUGHTS } from "../utils/queries";
+import {
+  Box,
+  SimpleGrid,
+  Stack,
+  Heading,
+  Divider,
+  Button,
+  Text,
+  Image,
+  Card,
+  CardBody,
+  CardFooter,
+  Link as ChakraLink,
+} from "@chakra-ui/react";
 
 const Home = () => {
   const [randomMeals, setRandomMeals] = useState([]);
@@ -10,14 +23,16 @@ const Home = () => {
   useEffect(() => {
     const fetchRandomMeals = async () => {
       try {
-        const response = await fetch('https://www.themealdb.com/api/json/v2/9973533/randomselection.php');
+        const response = await fetch(
+          "https://www.themealdb.com/api/json/v2/9973533/randomselection.php"
+        );
         const data = await response.json();
 
-        console.log('API Response:', data);
+        console.log("API Response:", data);
 
         setRandomMeals(data.meals);
       } catch (error) {
-        console.error('Error fetching random meals:', error);
+        console.error("Error fetching random meals:", error);
       }
     };
 
@@ -25,43 +40,59 @@ const Home = () => {
   }, []);
 
   return (
-    <main>
-      <SimpleGrid spacing={4} templateColumns='repeat(auto-fill, minmax(200px, 1fr))'>
-        {randomMeals.slice(0,6).map((randomMeal) => (
-      <Card key={randomMeal.idMeal} maxW='300px'mx='auto' mb={16} border='2px' borderColor='gray.300' borderRadius='lg' p={4}>
-  <Link to={`/recipe/${randomMeal.idMeal}`}>
-  <Image
-      src={randomMeal.strMealThumb} 
-      alt={randomMeal.strMeal}
-      borderRadius='lg'
-    />
-  </Link>
-  <CardBody>
-    <Stack mt='6' spacing='3'>
-      <Text fontSize='3xl' align='center' color='gray.800' as="u">{randomMeal.strMeal}</Text>
-      <Text color='green.600' fontSize='2xl' align='center'>
-        {randomMeal.strCategory}
-      </Text>
-    </Stack>
-  </CardBody>
-  <Divider />
-  <CardFooter display='flex' justifyContent='center' alignItems='center'>
-      <ChakraLink
-        href={randomMeal.strYoutube}
-        target="_blank"
-        rel="noopener noreferrer"
-        textDecoration="underline"
-        fontWeight="bold"
-        color='blue'
-        align='center'
-      > 
-      Video Instructions
-      </ChakraLink>
-  </CardFooter>
-</Card>
+    <Box bg="brand.gray" pt={12} pb={12}>
+      <SimpleGrid columns={[1, 2, 4, 5]} spacing={4}>
+        {randomMeals.slice(0, 12).map((randomMeal) => (
+          <Card
+            key={randomMeal.idMeal}
+            maxW="300px"
+            mx="auto"
+            mb={1}
+            border="3px"
+            borderColor="gray.300"
+            borderRadius="lg"
+            p={2}
+            bg="brand.blue"
+          >
+            <Link to={`/recipe/${randomMeal.idMeal}`}>
+              <Image
+                src={randomMeal.strMealThumb}
+                alt={randomMeal.strMeal}
+                borderRadius="lg"
+              />
+            </Link>
+            <CardBody>
+              <Stack mt="6" spacing="3">
+                <Text fontSize="3xl" align="center" color="brand.black" as="u">
+                  {randomMeal.strMeal}
+                </Text>
+                <Text color="green.600" fontSize="2xl" align="center">
+                  {randomMeal.strCategory}
+                </Text>
+              </Stack>
+            </CardBody>
+            <Divider />
+            <CardFooter
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <ChakraLink
+                href={randomMeal.strYoutube}
+                target="_blank"
+                rel="noopener noreferrer"
+                textDecoration="underline"
+                fontWeight="bold"
+                color="blue"
+                align="center"
+              >
+                Video Instructions
+              </ChakraLink>
+            </CardFooter>
+          </Card>
         ))}
       </SimpleGrid>
-    </main>
+    </Box>
   );
 };
 
